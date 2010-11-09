217494330f26381714eab1a640449a2b0c22bb89
Daniël de Kok <me@danieldk.eu>
2010-11-09 16:10:12 +0100

    At first bits of the POS tagging chapter.

---
+++ hs/chap-tagging.hs
@@ -0,0 +1,17 @@
+type Token = String
+type Tag = String
+
+data TrainingInstance = TrainingInstance Token Tag
+                        deriving (Show)
+
+
+rsplit :: Eq a => a -> [a] -> ([a], [a])
+rsplit sep l = let (ps, xs, _) = rsplit_ sep l in
+               (ps, xs)
+
+rsplit_ :: Eq a => a -> [a] -> ([a], [a], Bool)
+rsplit_ sep = foldr (splitFun sep) ([], [], False)
+    where splitFun sep e (px, xs, True) = (e:px, xs, True)
+          splitFun sep e (px, xs, False)
+                   | e == sep = (px, xs, True)
+                   | otherwise = (px, e:xs, False)
